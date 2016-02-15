f2b6c4ab370245e37d5f80075f3163591138fcb6
jimmybgammyknee <jimmymbreen>
2016-02-15 13:54:13 +1030

    This is a test to show Al what version control can do for him

--- plot_allSamples_eurasia.r
+++ plot_allSamples_eurasia.r
@@ -3,10 +3,10 @@ library(ggplot2)
 library(RColorBrewer)
 
 # most if not all
-df <- read.csv('all_mostSamples.csv', header = TRUE)
+#df <- read.csv('all_mostSamples.csv', header = TRUE)
 
 # remove oldest samples to adjust scale
-#df <- read.csv('all_mostSamples_notOld.csv', header = TRUE)
+df <- read.csv('all_mostSamples_notOld.csv', header = TRUE)
 
 points <- data.frame("age" = df$Age, "lat" = df$Lat, "lon" = df$Lon)
 
@@ -14,3 +14,5 @@ al1 = get_googlemap(center = c(lon = 40, lat = 55), zoom = 3, color="bw", maptyp
 map = ggmap(al1)
 
 map + geom_point(data=points, aes(x=lon, y=lat, colour=age)) + scale_colour_gradientn(colours=c("darkred", "green")) + geom_jitter(width = 0.2, height = 0.2) + xlab("Longitude") + ylab("Latitude") + ggtitle("Ancient Sample Distribution in Eurasia")
+
+# this is lame
